03e1144e4e5bd3454793acb9ca9f5bdb3db831e5
vijayashreepd98 <52790991+vijayashreepd98@users.noreply.github.com>
2020-07-21 14:59:51 +0530

    Add files via upload

---
+++ nodecourse/eventBooking/src/models/likeModel.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const eventActivityModel = mongoose.model('eventStatus', {
+  eventName: {
+    type:String,  
+    trim:true,
+    required: true
+  },
+  userName : {
+      type:String,
+      required:true
+  },
+   status: {
+    type:Boolean,
+    default:false
+  }
+});
+module.exports =eventActivityModel ;
